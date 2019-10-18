c828cf1d5ff2b903ccb48d29899c8178b1400c30
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-10-18 14:01:42 -0300

    Create exercicio3.asm

---
+++ exercicio3.asm
@@ -0,0 +1,36 @@
+	.data 
+str1: .asciiz "Entre com um número: "
+str2: .asciiz "A raiz dele é: "
+	.text 
+	.globl main
+main:
+	# Print da entrada do inteiro
+	li $v0, 4
+	la $a0, str1
+	syscall
+	
+	# Pegando o inteiro do usuário
+	li $v0,5
+	syscall
+	move $s1, $v0 # s1 = x
+	
+	
+root: # Função que calcula a raiz de um número
+	add $s2, $zero, 1 # s2 = r
+	add $s3, $zero, 2 # s3 = d
+	add $s4, $zero, 4 # s4 = s
+	add $s5, $zero, 1 # s5 = t
+	j while
+	
+while: # Função para o laço do programa
+	add $s2, $s2,1
+	add $s3, $s3,2
+	add $s4, $s4, $s3
+	add $s4, $s4, 1
+	slt $s5, $s4, $s1
+	beq $s5,1,while
+	
+	
+	
+	
+	
