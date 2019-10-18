087a59532627f45aa9431140bde8fceea211ebbd
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-10-18 12:08:11 -0300

    Update exercicio2.asm

--- exercicio2.asm
+++ exercicio2.asm
@@ -1,8 +1,8 @@
-	.data 
+.data 
 str1: .asciiz "Entre com a temperatura: "
 str2: .asciiz "A temperatura não está no intervalo"
-str3: .ascii "A temperatura está no intervalo permitido"
-
+str3: .asciiz "A temperatura está no intervalo permitido"
+str4: .asciiz "Saindo..."
 	.text 
 	.globl main
 main:
@@ -15,9 +15,21 @@ main:
 	li $v0, 5
 	syscall 
 	move $s0, $v0  
+	add $t3, $zero, 41
+	add $t4, $zero, 19
+	# Remodelando: x > 19 e x < 41
+	slt $t1, $t3, $s0
+	slt $t2, $s0,$t4 
+	beq $t1, $t2, padrao
+	li $v0, 4
+	la $a0, str2
+	syscall
 	j fim
 	
-	
+padrao:
+	li $v0, 4
+	la $a0, str3
+	syscall	
 fim: 
 	li $v0, 10
 	syscall 
