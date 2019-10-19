8a5380eacb1f96ca8eaeb04c9d5fe8a09f1ff41c
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-10-19 20:21:24 -0300

    Update exercicio3.asm

--- exercicio3.asm
+++ exercicio3.asm
@@ -1,4 +1,4 @@
-	.data 
+.data 
 str1: .asciiz "Entre com um número: "
 str2: .asciiz "A raiz dele é: "
 	.text 
@@ -19,7 +19,7 @@ root: # Função que calcula a raiz de um número
 	add $s2, $zero, 1 # s2 = r
 	add $s3, $zero, 2 # s3 = d
 	add $s4, $zero, 4 # s4 = s
-	add $s5, $zero, 1 # s5 = t
+	#add $s5, $zero, 1 # s5 = t
 	j while
 	
 while: # Função para o laço do programa
@@ -29,8 +29,12 @@ while: # Função para o laço do programa
 	add $s4, $s4, 1
 	slt $s5, $s4, $s1
 	beq $s5,1,while
-	
-	
-	
-	
-	
+	j mostrar
+mostrar:
+	add $s2, $s2, 1
+	li $v0, 4
+	la $a0, str2
+	syscall
+	li $v0, 1
+	move $a0, $s2
+	syscall
